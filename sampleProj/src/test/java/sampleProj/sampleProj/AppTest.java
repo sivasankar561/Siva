6639a555fde86b5afd405fce87f3bb4289257d3c
Siva <Siva@192.168.0.7>
2019-11-22 20:08:27 +0530

    test

---
+++ sampleProj/src/test/java/sampleProj/sampleProj/AppTest.java
@@ -0,0 +1,20 @@
+package sampleProj.sampleProj;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	@Test
+	public void sampleMethod() {
+		System.out.println("Hello World!");
+	}
+
+}
